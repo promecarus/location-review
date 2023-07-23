619f257091950f589e4ab780b85bda09bea016b5
promecarus <haikalslipi@gmail.com>
2023-07-23 18:55:56 +0700

    progress

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,31 @@
 <?php
 
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => $request->user());
+Route::get('/', fn () => ['message' => 'Welcome to Location Review API.']);
+Route::get('/ping', fn () => ['message' => 'pong']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post(
+    '/login',
+    fn () => (Auth::attempt(request()->only('email', 'password')))
+        ? [
+            'token' => Auth::user()->createToken('auth_token')->plainTextToken
+        ]
+        : [
+            'message' => 'The provided credentials are incorrect.'
+        ]
+);
+
+Route::post('/register', [UserController::class, 'store']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('locations', LocationController::class);
+    Route::apiResource('reviews', ReviewController::class);
+    Route::apiResource('users', UserController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
-use App\Http\Controllers\LocationController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->group(function () {
-    Route::get('/', function () {
-        return response()->json([
-            'message' => 'Welcome to the Location Review API.'
-        ]);
-    });
-    Route::apiResource('locations', LocationController::class);
-    Route::apiResource('reviews', ReviewController::class);
-    Route::apiResource('users', UserController::class);
+Route::get('/', function () {
+    return view('welcome');
 });
